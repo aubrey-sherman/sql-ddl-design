e0af7c0cc327642c7bedd56b91423d1988f77890
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-04-30 20:12:16 -0400

    Created tables, inserted data and wrote query for the db.

--- outspace.sql
+++ outspace.sql
@@ -1,21 +1,25 @@
 CREATE table moons (
     moon_name VARCHAR(50) PRIMARY KEY,
-    planet_of_orbit VARCHAR(50) REFERENCES planets
-);
+    planet_of_orbit VARCHAR(50) REFERENCES planets);
+
+INSERT into moons (moon_name, planet_of_orbit) VALUES ('The Moon', 'Earth'), ('Phobos', 'Mars'), ('Deimos', 'Mars');
 
 CREATE table planets (
    planet_name VARCHAR(50) PRIMARY KEY,
     orbital_period FLOAT NOT NULL,
-        CHECK(orbital_period in ('years')),
-    star_of_orbit VARCHAR(50) REFERENCES stars
-);
+    star_of_orbit VARCHAR(50) REFERENCES stars);
 
+INSERT into planets (planet_name, orbital_period, star_of_orbit) VALUES ('Earth', 1.00, 'The Sun'), (
+    'Mars', 1.882, 'The Sun'), ('Venus', 0.62, 'The Sun'), ('Proxima Centauri b', 0.03, 'Proxima Centauri'), (
+        'Gliese 876 b', 0.236, 'Gliese 876');
 
 CREATE table stars (
     star_name VARCHAR(50) PRIMARY KEY,
-    temp INT NOT NULL,
-        CHECK(temp in ('Kelvin'))
-);
+    temp VARCHAR(50) NOT NULL
+        CHECK(temp LIKE ('%°K')));
+
+INSERT into stars (star_name, temp) VALUES ('The Sun', '5800°K'), ('Proxima Centauri', '3042°K'), ('Gliese 876', '3192°K');
+
 
 SELECT planet_name, star_name, COUNT(moon_name) AS moon_count
 FROM planets
@@ -24,4 +28,4 @@ JOIN moons
 JOIN stars
   ON star_of_orbit = star_name
 GROUP BY planet_name, star_name
-ORDER BY planet_name
+ORDER BY planet_name;
